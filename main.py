85734759095d6a1413c80acc742d343e2dcd24a1
Theo <theo.dev.1234567890@gmail.com>
2024-10-07 20:07:11 -0300

    v.3

--- main.py
+++ main.py
@@ -11,23 +11,30 @@ from Crypto.Random import get_random_bytes
 
 app = Flask(__name__)
 app.secret_key = b'c\xb3G*\xa3\xf1\xf1\xf2#\x0c\x0e\xa4"d$t'
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
+cross_origin_resource_sharing = CORS(app,origins=["http://localhost:3000"])
 terminalLogs = {}
+terminal_logs_saved = True
 
 with open('./terminalLogs.json','r') as file:
     terminalLogs = json.load(file)
     file.close()
 
 def applyTerminalLogs(message : str, weight : int):
-    terminalLogs[time.time()] = {'date':datetime.datetime.now(),'message':message,'weight':weight}
+    global terminal_logs_saved
+    terminalLogs[time.time()] = str({"date":datetime.datetime.now(),"message":message,"weight":weight})
+    terminal_logs_saved = False
     ## Weight: -1(error) 0(warn) 1(normal) 2(sucess)
 def __terminallogthreadfun___():
-    while True:
-        time.sleep(500)    
-        with open('./terminalLogs.json','w') as file:
-            json.dump(terminalLogs,file)
-            file.close()
+    global terminal_logs_saved
+    while True:   
+        if not terminal_logs_saved:
+            with open('./terminalLogs.json','w') as file:
+                json.dump(terminalLogs,file)
+                file.close()
+                terminal_logs_saved = True
+        time.sleep(1) 
         
         
 
@@ -41,44 +48,39 @@ with app.app_context():
 migrage = Migrate(app,db)
 migrage.init_app(app,db)
 
-class create:
-    def auth_key(self,length=500) -> str:
-        all_characters = str.split("1 2 3 4 5 6 7 8 9 0 ! @ # $ % * ( ) _ - = + § : ; ^ ~ { ] | / , . < > q w e r t y u i o p a s d f g h j k l z x c v b n m", " ")
-        result = ""
-        for _ in range(length):
-            if randint(0,1) == 1:
-                result += str.upper(secrets.choice(all_characters))
-            else:
-                result += str.lower(secrets.choice(all_characters))
-        return result
+def create_auth_key(length=500) -> str:
+        return get_random_bytes(450)
 class Crypt:
     def __init__(self) -> None:
         pass
     def encrypt(self,plain_text):
+        plain_text = str(plain_text).encode('utf-8')
         cipher = AES.new(app.secret_key,AES.MODE_EAX)
         text,tag =  cipher.encrypt_and_digest(plain_text)
         return text,tag,cipher.nonce
     def decrypt(self,cipher_text,tag,nonce):
+        cipher_text = str(cipher_text).encode('utf-8')
         cipher = AES.new(app.secret_key,AES.MODE_EAX,nonce=nonce)
         return cipher.decrypt_and_verify(cipher_text,tag)
 Crypting = Crypt()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer,nullable=False,primary_key=True,unique=True)
-    auth_key=db.Column(db.String(500),nullable=False,unique=True, default=create.auth_key)
+    auth_key=db.Column(db.String(500),nullable=False,unique=True, default=create_auth_key)
     super_user=db.Column(db.Boolean,nullable=False,default=False)
     
     email = db.Column(db.String(320**2),nullable=False,unique=True)
-    email_nonce = db.Column(db.String(32),nullable=False)
-    email_tag = db.Column(db.String(32),nullable=False)
+    email_nonce = db.Column(db.String(32**2),nullable=False)
+    email_tag = db.Column(db.String(32**2),nullable=False)
     
     password = db.Column(db.String(300**2),nullable=False)
     password_nonce = db.Column(db.String(320**2),nullable=False)
     password_tag = db.Column(db.String(320**2),nullable=False)
 
     expense_json = db.Column(db.Text,nullable=False)
-    expense_json_nonce = db.Column(db.String(32),nullable=False)
-    expense_json_tag = db.Column(db.String(32),nullable=False)
+    expense_json_nonce = db.Column(db.Text,nullable=False)
+    expense_json_tag = db.Column(db.Text,nullable=False)
 
 class data_manage:
     def __init__(self) -> None:
@@ -86,6 +88,7 @@ class data_manage:
     def create_user(self,email,password):
         try:
             new_user = User()
+            new_user.auth_key = get_random_bytes(400)
             json_data,json_tag,json_nonce = Crypting.encrypt(b'{}')
             
             new_user.expense_json = json_data
@@ -111,7 +114,7 @@ class data_manage:
             applyTerminalLogs('An error have ocurred while creating a new user, rollback have been made.',-1)
             applyTerminalLogs(str(err),-1)
             return None
-    def check_user_exists(email) -> bool:
+    def check_user_exists(self,email) -> bool:
         email_cipher,email_tag,email_nonce = Crypting.encrypt(str(email).encode("utf-8"))
         return (User.query.filter_by(
             email=email_cipher,
@@ -141,15 +144,16 @@ def login_user_method():
             auth_key = track_user.auth_key
         else:
             applyTerminalLogs("A received request[POST] with intentions[Login] has failed, the email or password was wrong",1)
+        print(auth_key)
         return jsonify({
                 'sucess' : sucess,
                 'auth_key': auth_key
             })
     else:
         if not 'email' in data:
-            applyTerminalLogs("A received request[POST] haven't a valid body : Missing 'email' in 'data'",0)
+            applyTerminalLogs("A received request[POST] haven't a valid body : Missing (email) in (data)",0)
         if not 'password' in data:
-            applyTerminalLogs("A received request[POST] haven't a valid body : Missing 'password' in 'data'",0)
+            applyTerminalLogs("A received request[POST] haven't a valid body : Missing (password) in (data)",0)
 data_m = data_manage()
 @app.route("/register_user",methods=['POST'])
 def register_user_request():
@@ -158,19 +162,21 @@ def register_user_request():
     if request.method == "POST" and 'email' in data and 'password' in data:
         sucess = False
         auth_key = ''
-        if not data_m.check_user_exists(data.get('email')):
-            new_user = data_m.create_user(data.get('email'),data.get('password'))
+        data_return = {'sucess':False,'auth_key':""}
+        if not data_m.check_user_exists(data['email']):
+            new_user = data_m.create_user(data['email'],data['password'])
             if new_user:
                 applyTerminalLogs("A new user has been created!",2)
                 auth_key = new_user.auth_key
                 sucess = True
-
-        return jsonify({'sucess':sucess,'auth_key':auth_key})
+        data_return['auth_key'] = str(auth_key)
+        data_return['sucess'] = bool(sucess)
+        return jsonify(data_return)
     else:
         if not 'email' in data:
-            applyTerminalLogs("A received request[POST] haven't a valid body : Missing 'email' in 'data'",0)
+            applyTerminalLogs("A received request[POST] haven't a valid body : Missing (email) in (data)",0)
         if not 'password' in data:
-            applyTerminalLogs("A received request[POST] haven't a valid body : Missing 'password' in 'data'",0)
+            applyTerminalLogs("A received request[POST] haven't a valid body : Missing (password) in (data)",0)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port=30)
